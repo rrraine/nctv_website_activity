23a83f8658caa80b8294cb3e3060c15531d9558f
Lorraine Quezada <lorraineequezada@gmail.com>
2026-07-03 13:13:53 +0800

    feat(app): compose home page sections in root app template

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { About } from './features/home/about/about';
+import { Footer } from './features/home/footer/footer';
+import { Hero } from './features/home/hero/hero';
+import { PodcastTestimonial } from './features/home/podcast-testimonial/podcast-testimonial';
+import { Services } from './features/home/services/services';
+import { NavBar } from './shared/components/nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavBar, Hero, About, Services, PodcastTestimonial, Footer, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('ncompass-clone');
-}
+export class App {}
